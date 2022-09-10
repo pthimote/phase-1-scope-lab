e43788cb5f2d742f04df3172aaf20cfc2211aad4
Paul Thimote <pthimote@gmail.com>
2022-09-10 15:44:39 -0400

    commit tree 31dff84b06af4874858018c66c2c0c9b9d1353a2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    // let customerName = 'bob'
+    customerName = customerName.toUpperCase()
+    
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'billy'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'paul'
+}
